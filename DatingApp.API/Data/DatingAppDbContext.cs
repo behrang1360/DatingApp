e9e73b82b3e3eb91494d9364a10c5d6575511127
behrang <behrang1360@gmail.com>
2019-11-16 14:27:06 +0100

    initialize project

---
+++ DatingApp.API/Data/DatingAppDbContext.cs
@@ -0,0 +1,14 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DatingAppDbContext : DbContext
+    {
+        public DatingAppDbContext(DbContextOptions<DatingAppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<value> Values { get; set; }
+    }
+}
